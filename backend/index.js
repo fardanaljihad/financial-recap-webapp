a08ad2238f30d4fca54c3a2c3750e9c2347dce81
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-10-06 15:34:57 +0700

    import routes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import cors from "cors";
 
+import UserRoute from "./routes/UserRoute.js";
+import IncomeRoute from "./routes/IncomeRoute.js";
+import WalletRoute from "./routes/WalletRoute.js";
+import OutcomeRoute from "./routes/OutcomeRoute.js";
+import JenisPengeluaranRoute from "./routes/JenisPengeluaranRoute.js";
+import CategoryRoute from "./routes/CategoryRoute.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(UserRoute);
+app.use(IncomeRoute);
+app.use(WalletRoute);
+app.use(OutcomeRoute);
+app.use(JenisPengeluaranRoute);
+app.use(CategoryRoute);
 
 app.listen(5000, ()=> console.log('Server up and running...'));
